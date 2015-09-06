a7f451eaa36041b07b4c10f3fed4ffd453952949
Dalton Woodard <daltonmwoodard@gmail.com>
2015-09-05 19:12:38 -0700

    some test cases for deferred function calls

--- test/src/defer.cpp
+++ test/src/defer.cpp
@@ -1,5 +1,34 @@
+
+#include <iostream>
+
+#include "defer.hpp"
+
+struct foo
+{
+    std::string bar;
+};
+
+void function (int const& x)
+{
+    std::cout << "got a number: " << x << std::endl;
+}
+
 int main (void)
 {
+    auto arg1 = 1;
+    auto arg2 = 2;
+    auto deferred1 = funk::defer (function, arg1);
+    std::cout << "just deferred 'function' with argument: " << arg1 << "." << std::endl;
+    std::cout << "when called not deferred with argument: " << arg2 << "." << std::endl;
+    function (arg2);
+    std::cout << "when called deferred:" << std::endl;
+    deferred1 ();
+    
+    // when defered with objects that don't exist at deferred call time.
+    auto deferred2 = funk::defer (&foo::bar, foo {"test"});
+
+    std::cout << "calling deferred function with argument that no longer exists:" << std::endl;
+    std::cout << deferred2() << std::endl;
     return 0;
 }
 
