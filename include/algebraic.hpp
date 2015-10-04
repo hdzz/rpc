0730144ddaea68fd50657cd54c97593a8229b989
Dalton Woodard <daltonmwoodard@gmail.com>
2015-10-04 01:53:39 -0700

    implemented full support for recursive algebraic data types

--- include/algebraic.hpp
+++ include/algebraic.hpp
@@ -66,6 +66,64 @@ namespace detail
         alignas (std::max({alignof(T), alignof(Ts)...})) unsigned char data [std::max({sizeof(T), sizeof(Ts)...})];
     };
 } // namespace detail
+    template <typename T, class Alloc = std::allocator<T>>
+    struct recursive
+    {
+    public:
+        using type            = T;
+        using reference       = T&;
+        using const_reference = T const&;
+        using pointer         = T*;
+        using const_pointer   = T const*;
+
+        recursive (void) = default;
+
+        recursive (T && t) : data_ (alloc_.allocate (1))
+        {
+            new (data_.get()) T (t);
+        }
+
+        recursive (T const& t) : data_ (alloc_.allocate (1))
+        {
+            new (data_.get()) T (t);
+        }
+
+        recursive (recursive<T> && r)     : data_ (std::move(r.ptr())) {}
+ 
+        recursive (recursive<T> const& r) : data_ (alloc_.allocate (1))
+        {
+            new (data_.ptr()) T (r.get());
+        }
+
+        ~recursive (void) noexcept = default;
+ 
+        inline constexpr T&       value (void) &      { return *addressof(); }
+        inline constexpr T&&      value (void) &&     { return std::move (*addressof()); }
+        inline constexpr T const& value (void) const& { return *addressof(); }
+ 
+        inline constexpr T&       operator* (void) &      { return value(); }
+        inline constexpr T&&      operator* (void) &&     { return value(); }
+        inline constexpr T const& operator* (void) const& { return value(); }
+ 
+        inline constexpr T*       addressof (void) noexcept       { return reinterpret_cast<T*> (data_.get()); }
+        inline constexpr T const* addressof (void) const noexcept { return reinterpret_cast<T const*> (data_.get()); }
+ 
+        inline constexpr T*       operator& (void) noexcept       { return addressof(); }
+        inline constexpr T const* operator& (void) const noexcept { return addressof(); }
+
+        inline constexpr T* ptr (void) noexcept
+        {
+            return data_.get();
+        }
+
+        inline constexpr T const* ptr (void) const noexcept
+        {
+            return data_.get();
+        }
+    private:
+        Alloc alloc_;
+        std::unique_ptr<T> const data_; // recursive_type<T> is owning, as it merely replaces the existence of T in a variant.
+    };
 
     template <typename T, typename ... Ts>
     struct adt
@@ -73,14 +131,26 @@ namespace detail
     public:
         adt (void) : type_index_(0) { static_assert (sizeof(T) == 0, "cannot default construct algebraic data type"); }
 
-        adt (adt<T, Ts...> const&) = default;
-
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /* dummy template parameter to deal with overloading */>
         adt (U && u) : type_index_ (utility::type_to_index<U, T, Ts...>::value)
         {
             new (reinterpret_cast<void*> (storage.template addressof<U>())) U(std::forward<U>(u));
         }
+ 
+        template <typename U,
+            typename =
+                std::enable_if_t
+                    <fnk::utility::any_true(std::is_same<recursive<std::decay_t<U>>,T>::value,
+                                            std::is_same<recursive<std::decay_t<U>>,Ts>::value...)>>
+        adt (U && u) : type_index_ (utility::type_to_index<recursive<std::decay_t<U>>, T, Ts...>::value)
+        {
+            using W = std::decay_t<U>;
+            new (reinterpret_cast<void*> (storage.template addressof<W>())) recursive<W> (std::forward<U> (u));
+        }
+ 
+        adt (adt<T, Ts...> const&) = default;
         
         ~adt (void) noexcept = default;
 
@@ -117,56 +187,132 @@ namespace detail
         }
 
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>> 
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/> 
         inline constexpr U&       value (void) &      { return *addressof<U>(); }
-        
+ 
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        inline constexpr U&       value (void) &      { return **addressof<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U&&      value (void) &&     { return std::move (*addressof<U>()); }
-        
+ 
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>>
+        inline constexpr U&&      value (void) &&     { return std::move (**addressof<recursive<U>>()); } 
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/> 
         inline constexpr U const& value (void) const& { return *addressof<U>(); }
-       
+  
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>>
+        inline constexpr U const& value (void) const& { return **addressof<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U&       operator* (void) &      { return value<U>(); }
-        
+   
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        inline constexpr U&       operator* (void) &      { return value<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U&&      operator* (void) &&     { return value<U>(); }
-        
+    
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        inline constexpr U&&      operator* (void) &&     { return value<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U const& operator* (void) const& { return value<U>(); }
-        
+     
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        inline constexpr U const& operator* (void) const& { return value<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U*       addressof (void) noexcept
         {
             return reinterpret_cast<U*> (storage.template addressof<U>());
         }
-        
+      
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>>   
+        inline constexpr U*       addressof (void) noexcept
+        {
+            return reinterpret_cast<U*> (storage.template addressof<recursive<U>>()->addressof());
+        }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
         inline constexpr U const* addressof (void) const noexcept
         {
             return reinterpret_cast<U const*> (storage.template addressof<U>());
         }
-        
-        template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
-        inline constexpr U*       operator& (void) noexcept       { return addressof<U>(); }
-        
-        template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
-        inline constexpr U const* operator& (void) const noexcept { return addressof<U>(); }
        
         template <typename U,
-            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>>
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>>  
+        inline constexpr U const* addressof (void) const noexcept
+        {
+            return reinterpret_cast<U const*> (storage.template addressof<recursive<U>>()->addressof());
+        }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>       
+        inline constexpr U*       operator& (void) noexcept       { return addressof<U>(); }
+ 
+        template <typename U,
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>>
+        inline constexpr U*       operator& (void) noexcept       { return addressof<recursive<U>>(); }
+
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>
+        inline constexpr U const* operator& (void) const noexcept { return addressof<U>(); }
+  
+        template <typename U,
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        inline constexpr U const* operator& (void) const noexcept { return addressof<recursive<U>>(); }
+ 
+        template <typename U,
+            typename = std::enable_if_t<fnk::utility::any_true(std::is_same<U,T>::value, std::is_same<U,Ts>::value...)>,
+            bool _ = bool{} /*dummy template parameter to deal with overloading*/>      
         constexpr decltype(auto) operator= (U && u) noexcept (std::is_nothrow_move_assignable<U>::value)
         {
             new (reinterpret_cast<void*> (storage.template addressof<U>())) U(std::forward<U>(u));
+            return *this;
+        }
+   
+        template <typename U,
+            typename =
+                std::enable_if_t<fnk::utility::any_true(std::is_same<recursive<U>,T>::value, std::is_same<recursive<U>,Ts>::value...)>> 
+        constexpr decltype(auto) operator= (U && u) noexcept (std::is_nothrow_move_assignable<U>::value)
+        {
+            new (reinterpret_cast<void*> (storage.template addressof<recursive<U>>())) recursive<U> (std::forward<U>(u));
             return *this;
         }
 
@@ -212,6 +358,13 @@ namespace detail
     
     template <typename T, typename ... Ts>
     struct is_algebraic<adt<T, Ts...> &&> : public std::true_type {};
+
+    struct foo;
+
+    struct foo
+    {
+        adt<recursive<foo>, int> data;
+    };
 } // namespace fnk
 
 #endif // ifndef ALGEBRAIC_HPP
