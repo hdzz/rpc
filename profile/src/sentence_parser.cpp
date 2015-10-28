971b457a0c83439267673b3ae7e8fa885514b839
Dalton Woodard <daltonmwoodard@gmail.com>
2015-10-27 20:27:59 -0700

    some updates to profiling code

--- profile/src/sentence_parser.cpp
+++ profile/src/sentence_parser.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 #include <streambuf>
 #include <utility>
@@ -16,27 +17,49 @@
 #include "core/range.hpp"
 #include "core/parser.hpp"
 #include "core/combinators.hpp"
-#include "basic/atom_parsers.hpp"
+#include "core/token_parsers.hpp"
 #include "basic/text_parsers.hpp"
 #include "basic/regex_parsers.hpp"
 
+#ifndef RPC_PRINT_RESULTS
+#define RPC_PRINT_RESULTS 0
+#else
+#define RPC_PRINT_RESULTS 1
+#endif
+
 using namespace rpc;
+using namespace rpc::core;
+using namespace rpc::basic;
 
-template <typename T>
-using iter_type = typename std::basic_string<T>::const_iterator;
+using iter = typename std::basic_string<char>::const_iterator;
 
-template <typename V, typename T = V>
-using strparser = core::parser<iter_type<T>, V>;
+using sentence_type = std::deque<std::string>;
 
-strparser<std::string, char> sentence =
-    rpc::core::sequence
-        (rpc::basic::words<iter_type<char>>,
-         rpc::core::lift
-            (rpc::basic::punct<iter_type<char>>, [](char c) { return std::string (1, c); }));
+template <typename T, typename C>
+auto accumulate_back = [](T const& t, C & c)
+{
+    c.push_back (t);
+    return c;
+};
 
-auto sentences = rpc::core::some (rpc::core::ignorel (rpc::basic::spacem<iter_type<char>>, sentence));
+template <typename T, typename C>
+auto accumulate_front = [](T const& t, C & c)
+{
+    c.push_front (t);
+    return c;
+};
 
-inline bool file_exists (std::string const& filename)
+auto wordsep  = ignorer (word<iter>, spacem<iter>);
+auto punctstr = lift
+    (punct<iter>, [](char c) { return std::string (1, c); });
+auto sentence = lift<sentence_type>
+    (reducer (sequence (some (wordsep), punctstr),
+             accumulate_front<std::string, std::deque<std::string>>,
+             std::deque<std::string>{}));
+auto sentencesep = ignorer (sentence, spacem<iter>);
+auto sentences = some (sentencesep);
+
+bool file_exists (std::string const& filename)
 {
     std::ifstream f (filename);
     return f.good();
@@ -51,8 +74,24 @@ std::string read_in_file (std::string const& filename)
     out.reserve (file.tellg());
     file.seekg (0, std::ios::beg);
 
-    out.assign ((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    out.assign ((std::istreambuf_iterator<char>(file)),
+                 std::istreambuf_iterator<char>());
+    out.erase  (1 + out.find_last_not_of (" \v\n\r\t"));
     return out;
+}
+
+template <typename T>
+std::string print_results (std::deque<T> const& l)
+{
+    std::stringstream out;
+    if (not l.empty()) {
+        out << "[";
+        for (auto it = l.cbegin(); it != std::prev(l.cend()); ++it)
+            out << fnk::utility::to_string<T> (*it) + ", ";
+        out << fnk::utility::to_string<T>(l.back()) + "]";
+    } else
+        out << "[]";
+    return out.str();
 }
 
 int main (int argc, char ** argv)
@@ -68,7 +107,10 @@ int main (int argc, char ** argv)
     filename.assign (argv[1]);
     
     if (not file_exists (filename)) {
-        std::cout << "File: " << filename << " does not exist (or cannot be read)!" << std::endl;
+        std::cout << "File: "
+                  << filename
+                  << " does not exist (or cannot be read)!"
+                  << std::endl;
         std::exit (EXIT_FAILURE);
     }
 
@@ -76,17 +118,36 @@ int main (int argc, char ** argv)
     std::cout << "Parsing: " << filename << " for sentences\n..." << std::endl;
     {
         auto start = std::chrono::high_resolution_clock::now();
-        auto res   = sentences.parse (parse_text);
+        auto res   = rpc::core::parse (sentences, parse_text);
         auto end   = std::chrono::high_resolution_clock::now();
         std::cout
             << "parse result: "
-            << [](bool b) { return b ? "success" : "failure"; } (sentences.is_parse_success (res))
+            << [](bool b) { return b ? "success" : "failure"; }
+                    (rpc::core::parse_success (res))
             << std::endl;
+        if (not rpc::core::parse_success (res)) {
+            for (auto e : res)
+                if (parse_failure (e)) {
+                    std::cout << '\t'
+                              << rpc::core::toresult_failure_message (e)
+                              << " at: '"
+                              << rpc::core::torange (e).grab (10)
+                              << "'"
+                              << std::endl;
+                    std::cout << "\tparse data:"
+                              << print_results (core::values(res)) << std::endl;
+                }
+        } else if (RPC_PRINT_RESULTS) {
+            std::cout << "original data: "
+                      << parse_text << std::endl;
+            std::cout << "parsed data: "
+                      << print_results (core::values (res)) << std::endl;
+        }
         std::cout << "elapsed time: "
-              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << std::chrono::duration_cast<std::chrono::microseconds>
+                    (end - start).count()
               << " microsec." << std::endl;
     }
 
-    std::exit (EXIT_SUCCESS);
+    return 0;
 }
-
